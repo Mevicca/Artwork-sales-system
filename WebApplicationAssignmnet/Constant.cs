bed414b0c2d95b2de5e449b76f7538c8a2004acb
mevicca <mevicca@gmail.com>
2021-02-06 01:13:30 +0800

    Done Login & Registration page

---
+++ WebApplicationAssignmnet/Constant.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplicationAssignmnet
+{
+    public class Constant
+    {
+        public const string connectionStrings = "Data Source = (LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\Mevicca\\source\\repos\\Mevicca\\WebAssignment\\WebApplicationAssignmnet\\App_Data\\Tayy.mdf;Integrated Security = True;";
+    }
+}
---
+++ WebApplicationAssignmnet/lib/js/SignUp.js
@@ -0,0 +1,35 @@
+﻿$(window).on('load', function () {
+    adjustContainerPadding();
+});
+
+$(window).resize(function () {
+    adjustContainerPadding();
+});
+
+function adjustContainerPadding() {
+    //For the content height
+    var screen = window.innerHeight;
+    var header = parseInt(document.querySelector('header').offsetHeight);
+    var footerHeight = document.getElementById("footer").offsetHeight;
+    var newHeight = (screen - header - footerHeight);
+    $(".card").css("height", '430px');
+
+    if (newHeight - 430 > 0) {
+        var paddingTable = (newHeight - 430) / 2;
+        $(".col-md-6").css("margin-top", paddingTable);
+        $(".col-md-6").css("margin-bottom", paddingTable);
+    }
+    else {
+        newHeight = screen - header + footerHeight;
+    }
+
+    $("#content").css("height", newHeight);
+
+    //For the scroll control
+    var footerHeight = document.getElementById("footer").offsetHeight;
+    $("#nav-bar").css("margin-bottom", footerHeight);
+
+    if (window.innerHeight < 340) {
+        $("#nav-bar").css("margin-bottom", "200px");
+    }
+}
