e0eb7bf4d73123011649ce50e6157b3b245cb42f
Bong YIn Ying <bongyy-wm18@student.tarc.edu.my>
2021-02-18 22:47:28 +0800

    Add homepage and FAQ page

---
+++ WebApplicationAssignmnet/lib/js/Homepage.js
@@ -0,0 +1,46 @@
+﻿var slideIndex = 1, slideAuto = 0;
+autoSlides();
+showSlides(slideIndex);
+
+function autoSlides() {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideAuto++;
+    if (slideAuto > slides.length) { slideAuto = 1 }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideAuto - 1].style.display = "block";
+    dots[slideAuto - 1].className += " active";
+    setTimeout(autoSlides, 3000); // Change image every 3 seconds
+}
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) { slideIndex = 1 }
+    if (n < 1) { slideIndex = slides.length }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex - 1].style.display = "block";
+    dots[slideIndex - 1].className += " active";
+}
