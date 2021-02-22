ef1c6554eb6208afad4dbb9d1b71527592bcc9c2
mevicca <mevicca@gmail.com>
2021-02-22 10:07:42 +0800

    solve the artist logout problem

--- WebApplicationAssignmnet/WebArt.Master.designer.cs
+++ WebApplicationAssignmnet/WebArt.Master.designer.cs
@@ -15,12 +15,12 @@ namespace WebApplicationAssignmnet
     {
 
         /// <summary>
-        /// ContentPlaceHolder1 control.
+        /// ContentPlaceHolder control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
-        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
+        protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder;
     }
 }
